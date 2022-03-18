8944aad18935ad824ad975384ffff00515d70252
Juliana Dantas <julianadantas498@gmail.com>
2022-03-18 02:23:38 -0300

    update

--- otimTSP/src/main.cpp
+++ otimTSP/src/main.cpp
@@ -22,13 +22,112 @@ struct InsertionInfo{
 };
 
 struct Solution{
-  std::vector<int> sequence;
+  vector<int> sequence;
   double custoSolucao;
 };
+
+bool compares(InsertionInfo a, InsertionInfo b);
+
+std:: vector<InsertionInfo> calcularCustoInsercao (Solution& s, vector<int>& CL);
+
+Solution Construcao(vector<int> CL);
+
+bool BestImprovementSwap (Solution& s);
+
+bool BestImprovement2Opt (Solution& s);
+
+bool BestImprovementOrOpt (Solution& s, int quantity);
+
+void BuscaLocal (Solution& s);
+
+Solution Pertubacao (Solution s);
+
+int main(int argc, char** argv) {
+
+    clock_t start= clock();    //inicia a contagem do tempo de execucao  
+
+    Solution s, bestS, bestOfAll;
+    int maxIter, maxIterIls;
+    int i, count, j;
+    vector<int> CL;
+
+    readData(argc, argv, &dimension, &matrizAdj);
+
+
+    if(dimension <= 150){
+      maxIterIls= dimension/ 2.0;
+    }else{
+      maxIterIls= dimension;
+    }
+    srand(time(NULL));
+
+    for(i= 2; i <= dimension; i++){ //comeca do 2 pq a c1 é adicionada na s.sequence na construcao
+      CL.push_back(i);
+    }
+
+   bestOfAll.custoSolucao= (double) INFINITY;
+   maxIter= 50;
+   for(i= 0; i < maxIter; i++){
+      
+      s= Construcao(CL);
+
+      bestS= s;
+
+      count= 0;
+      
+
+      while(count < maxIterIls){
+        BuscaLocal(s);
+        
+        if(s.custoSolucao < bestS.custoSolucao){
+          bestS= s;
+          count= 0;
+        }
+        // for(j= 0; j < s.sequence.size(); j++){
+        //   cout << s.sequence[j] << " ";
+        // }
+        // cout << endl;
+          s= Pertubacao(bestS);  
+         
+        // for(j= 0; j < s.sequence.size(); j++){
+        //   cout << s.sequence[j] << " ";
+        // }
+        // cout << s.custoSolucao << endl;
+        count++;
+      }
+
+    if(bestS.custoSolucao < bestOfAll.custoSolucao){
+      bestOfAll= bestS;
+    }
+     
+    
+   }
+    // printData();
+    cout << "melhor " << bestOfAll.custoSolucao << endl;
+    
+    clock_t end= clock();
+    double time= ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    cout << "tempo: " << time << endl;   //imprime o tempo total de execucao
+
+    return 0;  
+
+}
+
+void printData() {
+  cout << "dimension: " << dimension << endl;
+  for (size_t i = 1; i <= dimension; i++) {
+    for (size_t j = 1; j <= dimension; j++) {
+      cout << matrizAdj[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
 
 bool compares(InsertionInfo a, InsertionInfo b){  //funcao auxiliar na ordenacao dos custos
   return a.custo < b.custo;
 }
+
 
 std:: vector<InsertionInfo> calcularCustoInsercao (Solution& s, std::vector<int>& CL){
 
@@ -56,7 +155,7 @@ std:: vector<InsertionInfo> calcularCustoInsercao (Solution& s, std::vector<int>
   return custoInsercao;
 }
 
-Solution Construcao(std::vector<int> CL){
+Solution Construcao(vector<int> CL){
   
   int indexRandom, i, j;  
   Solution s;
@@ -65,21 +164,28 @@ Solution Construcao(std::vector<int> CL){
   s.sequence.push_back(1);      //adicionando a c1 no inicio e no final
 
 
-  for(i= 1; i <= 3; i++){       //funcao para escolher 3 cidades aleatorias
+  //escolher 3 cidades aleatorias
       
     indexRandom= rand() % CL.size();
-
     s.sequence.insert(s.sequence.end()-1, CL[indexRandom]);
     CL.erase(CL.begin() + indexRandom);
-  }
+
+    indexRandom= rand() % CL.size();
+    s.sequence.insert(s.sequence.end()-1, CL[indexRandom]);
+    CL.erase(CL.begin() + indexRandom);
+
+    indexRandom= rand() % CL.size();
+    s.sequence.insert(s.sequence.end()-1, CL[indexRandom]);
+    CL.erase(CL.begin() + indexRandom);
+
+    //
 
   while(!CL.empty()){
 
     std::vector<InsertionInfo> custoInsercao= calcularCustoInsercao(s, CL);
-    
-    
-    sort(custoInsercao.begin(), custoInsercao.end(), compares);   //ordena os custos
 
+    sort(custoInsercao.begin(), custoInsercao.end(), compares);   //ordena os custos
+    
     double alpha= (double) rand() / RAND_MAX;
     int selecionado= rand() % ( (int) ceil(alpha * custoInsercao.size()) );
 
@@ -134,7 +240,7 @@ bool BestImprovementSwap (Solution& s){         //estrutura de vizinhança: SWAP
   }
 
   if (bestDelta < 0){
-    std::swap(s.sequence[best_i], s.sequence[best_j]);
+    swap(s.sequence[best_i], s.sequence[best_j]);
     s.custoSolucao= s.custoSolucao + bestDelta;
     return true;
   }
@@ -172,7 +278,7 @@ bool BestImprovement2Opt (Solution& s){         //estrutura de vizinhança: 2opt
 
     j= best_j;
     for(i= best_i; i < j; i++){               //for para inverter a subsequencia obtida anteriormente
-      std::swap(s.sequence[i], s.sequence[j]);
+      swap(s.sequence[i], s.sequence[j]);
       j--;
     }
     s.custoSolucao= s.custoSolucao + bestDelta;
@@ -215,6 +321,7 @@ bool BestImprovementOrOpt (Solution& s, int quantity){   //as 3 outras estrutura
           }
         }
       }
+
       if (bestDelta < 0){
         s.sequence.insert(s.sequence.begin() + best_j + 1, s.sequence[best_i]);
         s.sequence.erase(s.sequence.begin() + best_i);
@@ -243,6 +350,7 @@ bool BestImprovementOrOpt (Solution& s, int quantity){   //as 3 outras estrutura
           }
         }
       }
+
       if (bestDelta < 0){
         s.sequence.insert(s.sequence.begin() + best_j + 2, s.sequence[best_i]);
         s.sequence.insert(s.sequence.begin() + best_j + 3, s.sequence[best_i+1]);
@@ -274,6 +382,7 @@ bool BestImprovementOrOpt (Solution& s, int quantity){   //as 3 outras estrutura
         }
         
       }
+
       if (bestDelta < 0){
         s.sequence.insert(s.sequence.begin() + best_j + 3, s.sequence[best_i]);
         s.sequence.insert(s.sequence.begin() + best_j + 4, s.sequence[best_i+1]);
@@ -292,8 +401,7 @@ bool BestImprovementOrOpt (Solution& s, int quantity){   //as 3 outras estrutura
 
 void BuscaLocal (Solution& s){
 
-  std::vector<int>NL= {1, 2, 3, 4, 5};
-  
+  vector<int>NL= {1, 2, 3, 4, 5};
   bool improved= false;
   
   while(NL.empty() == false){
@@ -302,19 +410,19 @@ void BuscaLocal (Solution& s){
 
     switch (NL[n]) {
       case 1: 
-        improved= BestImprovementSwap(s);      
+        improved= BestImprovementSwap(s);    
         break;
       case 2: 
-        improved= BestImprovement2Opt(s);      
+        improved= BestImprovement2Opt(s);  
         break;
       case 3:
         improved= BestImprovementOrOpt(s, 1);   //reinsertion    
         break;
       case 4:
-        improved= BestImprovementOrOpt(s, 2);   //Or-opt2       
+        improved= BestImprovementOrOpt(s, 2);   //Or-opt2      
         break;
       case 5:
-        improved= BestImprovementOrOpt(s, 3);   //Or-opt3       
+        improved= BestImprovementOrOpt(s, 3);   //Or-opt3      
         break;
     }
     
@@ -335,15 +443,13 @@ Solution Pertubacao (Solution s){
   int subseq1, subseq2;
   int index1, index2;
   int i, times;
-  int large, small;
   bool changed= false;
+  int large, small;
 
-  if (dimension <= 20){
-    subseq1= 2;
-    subseq2= 2;
+  if (dimension <= 29){
+    subseq1= subseq2= 2;
 
-    large= subseq1;
-    small= subseq1;
+    large= small= subseq1;
   }else{
      subseqMax= dimension / 10;
      subseqMax--;       //recurso utilizado para facilitar o uso do rand
@@ -369,12 +475,13 @@ Solution Pertubacao (Solution s){
     }
   }                                            
   while(1){
-    index2= rand() % dimension;
+    index2= rand() % dimension + index1 + subseq1;      //pq o index2 tem q ser sorteado a partir do indice index1+subseq1;
 
-    if(index2 >= index1+subseq1 && index2 <= dimension-subseq2){
+    if(index2 <= dimension-subseq2){
       break;
     }
   }
+
   
   if( (index1 + subseq1) == index2 ){
     s.custoSolucao= s.custoSolucao - matrizAdj[s.sequence[index1-1]][s.sequence[index1]] - matrizAdj[s.sequence[index1+subseq1-1]][s.sequence[index2]]
@@ -389,7 +496,7 @@ Solution Pertubacao (Solution s){
   }
 
   for(i= 0; i < small; i++){
-    std::swap(s.sequence[index1+i], s.sequence[index2+i]);
+    swap(s.sequence[index1+i], s.sequence[index2+i]);
   }
 
 
@@ -418,84 +525,4 @@ Solution Pertubacao (Solution s){
     }
 
   return s;
-}
-
-int main(int argc, char** argv) {
-
-    clock_t start= clock();    //inicia a contagem do tempo de execucao  
-
-    Solution s, bestS, bestOfAll;
-    int maxIter, maxIterIls;
-    int i, count, j;
-    std::vector<int> CL;
-
-    readData(argc, argv, &dimension, &matrizAdj);
-
-
-    if(dimension <= 150){
-      maxIterIls= dimension/ 2.0;
-    }else{
-      maxIterIls= dimension;
-    }
-    srand(time(NULL));
-
-    for(i= 2; i <= dimension; i++){ //comeca do 2 pq a c1 é adicionada na s.sequence na construcao
-      CL.push_back(i);
-    }
-
-   bestOfAll.custoSolucao= (double) INFINITY;
-   maxIter= 50;
-   for(i= 0; i < maxIter; i++){
-      
-      s= Construcao(CL);
-
-      bestS= s;
-
-      count= 0;
-      while(count < maxIterIls){
-        BuscaLocal(s);
-        
-        if(s.custoSolucao < bestS.custoSolucao){
-          bestS= s;
-          count= 0;
-        }
-        // for(j= 0; j < s.sequence.size(); j++){
-        //   cout << s.sequence[j] << " ";
-        // }
-        // cout << endl;
-          s= Pertubacao(bestS);  
-         
-        // for(j= 0; j < s.sequence.size(); j++){
-        //   cout << s.sequence[j] << " ";
-        // }
-        // cout << endl;
-        // cout << s.custoSolucao << endl;
-        count++;
-      }
-
-    if(bestS.custoSolucao < bestOfAll.custoSolucao){
-      bestOfAll= bestS;
-    }
-    
-   }
-    printData();
-    cout << "melhor " << bestOfAll.custoSolucao << endl;
-    
-    clock_t end= clock();
-    double time= ((double) (end - start)) / CLOCKS_PER_SEC;
-
-    cout << "tempo: " << time << endl;   //imprime o tempo total de execucao
-
-    return 0;  
-
-}
-
-void printData() {
-  cout << "dimension: " << dimension << endl;
-  for (size_t i = 1; i <= dimension; i++) {
-    for (size_t j = 1; j <= dimension; j++) {
-      cout << matrizAdj[i][j] << " ";
-    }
-    cout << endl;
-  }
 }
